3e24f7633eb0c227e8b52cab4940d6725d9287a0
Nullivex <contact@nullivex.com>
2013-03-12 22:36:07 -0700

    refactored func.php into packages, renamed functions, updated error handling, upgraded boot process

---
+++ func/mda_glob.php
@@ -0,0 +1,31 @@
+<?php
+
+function get($name=false){
+	if($name === false) return $_GET;
+	return mda_get($_GET,$name);
+}
+
+function post($name=false){
+	if($name === false) return $_POST;
+	return mda_get($_POST,$name);
+}
+
+function req($name=false){
+	if($name === false) return $_REQUEST;
+	return mda_get($_REQUEST,$name);
+}
+
+function server($name=false){
+	if($name === false) return $_SERVER;
+	return mda_get($_SERVER,$name);
+}
+
+function session($name,$value=false){
+	if($value !== false) $_SESSION[$name] = $value;
+	return mda_get($_SESSION,$name);
+}
+
+function session_delete(){
+	$args = func_get_args();
+	foreach($args as $name) unset($_SESSION[$name]);
+}
